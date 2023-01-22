6970733c86efc03a0d6c1b31b78c972df7b78803
Aleksy <aslepaugo@gmail.com>
2023-01-22 11:38:08 +0100

    Extend data validation for def register_order.

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -2,6 +2,7 @@ import json
 
 from django.http import JsonResponse
 from django.templatetags.static import static
+from phonenumbers import is_valid_number, parse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -60,17 +61,49 @@ def product_list_api(request):
     })
 
 
+def validate_field(data: dict, name: str, field_type: type) -> str | None:
+    try:
+        if data[name] is None:
+            return f"{name} can't be empty"
+        if not isinstance(data[name], field_type):
+            return f'for {name} expected field type {field_type}'
+        if not data[name]:
+            return f"{name} can't be empty"
+    except KeyError:
+        return f'{name} is a required field'
+
+
 @api_view(['POST'])
 def register_order(request):
     order_data = request.data
     if not order_data:
-        return Response()
-    if 'products' not in order_data:
-        return Response(['products: required field'], status=400)
-    if not isinstance(order_data['products'], list):
-        return Response(['products: expected list of values'], status=400)
-    if not order_data['products']:
-        return Response(['products: list can''t be empty'], status=400)           
+        return Response(['Empty request'], status=400)
+    errors = []
+    error = validate_field(order_data, 'firstname', str)
+    if error:
+        errors.append(error)
+    error = validate_field(order_data, 'lastname', str)
+    if error:
+        errors.append(error)
+    error = validate_field(order_data, 'phonenumber', str)
+    if error:
+        errors.append(error)
+    elif not is_valid_number(parse(order_data['phonenumber'])):
+        errors.append('phone number is not valid')
+    error = validate_field(order_data, 'address', str)
+    if error:
+        errors.append(error)
+    error = validate_field(order_data, 'products', list)
+    if error:
+        errors.append(error)
+    else:
+        order_ids = set(item['product'] for item in order_data['products'])
+        db_ids = Product.objects.all().values_list('id', flat=True)
+        err_ids = order_ids.difference(db_ids)
+        for err_id in err_ids:
+            errors.append(f'products:  {err_id}')
+    if errors:
+        return Response(errors, status=400)
     new_order = Order.objects.create(
         first_name = order_data['firstname'],
         last_name = order_data['lastname'],
