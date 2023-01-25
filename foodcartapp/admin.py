2b3bac62259986b1a1f47e79ed74b1d124d31bcc
Aleksy <aslepaugo@gmail.com>
2023-01-25 21:36:13 +0100

    Back to orders from edit in admin.

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
 
 from .models import Order, OrderItem, Product
 from .models import ProductCategory
@@ -115,3 +117,16 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = [
         OrderItemInline,
     ]
+
+    def response_change(self, request, obj):
+        response = super().response_post_save_change(request, obj)
+        if (
+            "next" in request.GET and
+            url_has_allowed_host_and_scheme(
+                url=request.GET['next'],
+                allowed_hosts={request.get_host()},
+                require_https=request.is_secure()
+            )
+        ):
+            return HttpResponseRedirect(request.GET['next'])
+        return response
