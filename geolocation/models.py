a13df52cd9f6e6e033051bff23428d98287e505f
Aleksy <aslepaugo@gmail.com>
2023-02-11 15:28:45 +0100

    Fix geocoder issues.

--- geolocation/models.py
+++ geolocation/models.py
@@ -14,14 +14,14 @@ class Location(models.Model):
 
     latitude = models.DecimalField(
         verbose_name='Широта',
-        max_digits=8,
+        max_digits=9,
         decimal_places=6,
         db_index=True,
         null=True,
     )
     longitude = models.DecimalField(
         verbose_name='Долгота',
-        max_digits=8,
+        max_digits=9,
         decimal_places=6,
         db_index=True,
         null=True,
@@ -38,10 +38,14 @@ class Location(models.Model):
 
     def process_coordinates(self):
         geocoder = Yandex(api_key=settings.YANDEX_API_KEY)
-        _, coordinates = geocoder.geocode(self.raw_address)
-        self.latitude, self.longitude = coordinates
-        self.updated_at = timezone.now()
-        self.save()
+        try:
+            _, coordinates = geocoder.geocode(self.raw_address)
+        except TypeError:
+            coordinates = None
+        if coordinates:
+            self.latitude, self.longitude = coordinates
+            self.updated_at = timezone.now()
+            self.save()
 
     def __str__(self):
         return f"{self.raw_address} {self.latitude} {self.longitude} {self.updated_at}"
