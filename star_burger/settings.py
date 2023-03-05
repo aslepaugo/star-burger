9609d60ef85c95df9b576e9b9669383c76169333
Aleksy <aslepaugo@gmail.com>
2023-03-05 10:58:35 +0100

    Remove django extension.

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -28,7 +28,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "django_extensions",
     'debug_toolbar',
     'phonenumber_field',
     'rest_framework',
